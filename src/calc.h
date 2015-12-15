a9796d6bf61499d5c94cd47b55791eaeada5b5f5
luca <ilgaspa93@gmail.com>
2015-12-15 15:33:10 +0100

    progettazione classi: 1.0

--- src/calc.h
+++ src/calc.h
@@ -1,7 +1,54 @@
-typedef enum{ATOM, EXPR, SET, FORALL, EXISTS, ALLDIFF, SUM, COMP, COUNT, LET, BOOL2INT, ABS, FMIN, FMAX} nodeType;
-typedef enum{PARTYPE, VARTYPE, ARRAY1DTYPE, ARRAY2DTYPE, ARRAY3DTYPE, PAR1DTYPE, PAR2DTYPE,PAR3DTYPE} varType;
-typedef enum{NONE, INTTYPE, FLOATTYPE, BOOLTYPE} basicType;
 
-struct Expr_node {
-	int a;
-} Expr_node;
+enum Expr_type {ATOM, EXPR, FUN};
+enum ID_type {VAR, PAR, SET};
+enum VP_type {DIM_1, DIM_2, DIM_3};
+enum Lit_type {INT, FLOAT, BOOL};
+
+
+class Expr_node;	
+
+class binSet {
+	int setDim;
+	int* values;
+};
+
+class uValue {
+	Lit_type type;
+	union{
+		int iValue;
+		float fValue;
+		bool bValue;
+	};
+};
+
+class Atom {
+	ID_type type;
+	char* name;
+	VP_type dim;
+	union{
+		uValue* value;
+		binSet* set;
+	};
+};
+
+class Expr {
+	int oper;
+	int nops;
+	Expr_node** op;
+};
+
+class Func {
+	//body
+	int body; //tmp var
+};
+
+class Expr_node {
+	Expr_type type;
+	union{
+		Atom atom;
+		Expr expr;
+		Func func;
+	};
+};
+
+
