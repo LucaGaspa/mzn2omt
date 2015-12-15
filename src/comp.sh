1524af896567ed13c65a8fed091ddd5469eefbaf
LucaGaspa <ilgaspa93@gmail.com>
2015-12-15 16:40:38 +0100

    more header

--- src/comp.sh
+++ src/comp.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#/*********************************************************************** 
+# * comp.sh                                                             *
+# *                                                                     *
+# * Copyright (C) 2015 Luca Gasparetto                                  *
+# * All rights reserved.                                                *
+# *                                                                     *
+# * This software may be modified and distributed under the terms       *
+# * of the MIT license.  See the LICENSE file for details.              *
+# ***********************************************************************/
+
 flex lexer.l
 bison -d parser.y
 g++ -o ../bin/mzn2smt parser.tab.c lex.yy.c
